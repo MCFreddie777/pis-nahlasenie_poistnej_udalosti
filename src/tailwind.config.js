db04ee6dd0a579ebf5cc21feea333c2fac32c904
František Gič <frantisek.gic@gmail.com>
2020-04-23 10:59:27 +0200

    Implement Tailwind CSS Framework into the project

---
+++ src/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    theme: {
+        extend: {
+            width: {
+                '96': '24rem',
+                '112': '28rem',
+                '128': '32rem',
+                '144': '36rem',
+                '160': '40rem',
+            }
+        }
+    },
+    variants: {
+        padding: ['responsive', 'first', 'hover', 'focus', 'last'],
+        cursor: ['hover'],
+        outline: ['focus'],
+        background: ['hover', 'group-hover'],
+        border: ['focus', 'hover'],
+        textColor: ['hover', 'focus', 'group-hover'],
+    },
+    plugins: []
+};

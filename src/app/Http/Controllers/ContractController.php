2454c2e123bf145242bc893319e6c63ec832006a
František Gič <frantisek.gic@gmail.com>
2020-04-23 11:27:44 +0200

    Create Contract model along with controller

---
+++ src/app/Http/Controllers/ContractController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Contract;
+use Illuminate\Http\Request;
+
+class ContractController extends Controller
+{
+    public function index()
+    {
+        return view('contracts.index');
+//            ->with('contracts', Contract::all());
+    }
+}
